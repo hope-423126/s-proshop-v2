57d6588bfc79ea627a4f07296abc74bed70bb7d8
Hope <lemaakterfskwi@gmail.com>
2025-04-16 11:46:02 -0500

    updated:mongodb connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/new_connection/eCommerce");
+    const conn = await mongoose.connect("mongodb://localhost:27017");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
